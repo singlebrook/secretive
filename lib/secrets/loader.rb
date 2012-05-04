f5595d69c51745be260ba5c3fafbf0d1c10b0e2f
Reade Harris <readeharris@gmail.com>
2012-05-04 13:59:16 -0400

    Add guard and refactor selfs.

--- lib/secrets/loader.rb
+++ lib/secrets/loader.rb
@@ -1,35 +1,38 @@
 module Secrets
   class Loader
-    def self.environmentalize!(yaml_file, scope=nil)
-      vars = YAML.load(File.open(yaml_file))
-      return unless vars.present?
+    class << self
+      def environmentalize!(yaml_file, scope=nil)
+        vars = YAML.load(File.open(yaml_file))
+        return unless vars.present?
       
-      convert_to_env_vars(vars)
-      convert_to_env_vars(vars.fetch(scope)) if scope.present?
-    end
+        convert_to_env_vars(vars)
+        convert_to_env_vars(vars.fetch(scope)) if scope.present?
+      end
     
-    def self.for_heroku(yaml_file, scope=nil)
-      vars = YAML.load(File.open(yaml_file))
+      def for_heroku(yaml_file, scope=nil)
+        vars = YAML.load(File.open(yaml_file))
+        return unless vars.present?
       
-      heroku_string = convert_vars_to_string(vars)
-      heroku_string += convert_vars_to_string(vars.fetch(scope)) if scope.present?
-      heroku_string
-    end
-    
-    private
-    
-    def self.convert_to_env_vars(vars)
-      vars.each do |key, value|
-        ENV[key] = value unless value.respond_to?(:each) || value.nil?
+        heroku_string = convert_vars_to_string(vars)
+        heroku_string += convert_vars_to_string(vars.fetch(scope)) if scope.present?
+        heroku_string
       end
-    end
     
-    def self.convert_vars_to_string(vars)
-      string = ""
-      vars.each do |key, value|
-        string.concat("#{key}=#{value} ") unless value.respond_to?(:each) || value.nil?
+      private
+    
+      def convert_to_env_vars(vars)
+        vars.each do |key, value|
+          ENV[key] = value unless value.respond_to?(:each) || value.nil?
+        end
       end
-      string
-    end 
+    
+      def convert_vars_to_string(vars)
+        string = ""
+        vars.each do |key, value|
+          string.concat("#{key}=#{value} ") unless value.respond_to?(:each) || value.nil?
+        end
+        string
+      end 
+    end
   end
 end
