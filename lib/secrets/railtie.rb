fcb294a2340be48f540e4c24494ebb82166fe72e
Reade Harris <readeharris@gmail.com>
2012-05-07 12:54:14 -0400

    Don't initialize in production.

--- lib/secrets/railtie.rb
+++ lib/secrets/railtie.rb
@@ -1,7 +1,7 @@
 module Secrets
   class Railtie < ::Rails::Railtie
     config.before_initialize do
-      Secrets.environmentalize!(Rails.env)
+      Secrets.environmentalize!(Rails.env) unless Rails.env.production?
     end
     
     rake_tasks do
