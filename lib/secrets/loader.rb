5652d051efc48ad9405d2d7b0169e725f973e176
Leon Miller-Out <leon@singlebrook.com>
2012-05-14 10:53:02 -0400

    Don't puke if there's no secrets file

--- lib/secrets/loader.rb
+++ lib/secrets/loader.rb
@@ -2,39 +2,41 @@ module Secrets
   class Loader
     class << self
       def environmentalize!(yaml_file, scope=nil)
+        return unless File.exists?(yaml_file)
+
         vars = YAML.load(File.open(yaml_file))
-      
+
         convert_to_env_vars(vars)
         convert_to_env_vars(vars.fetch(scope)) if scope.present?
       end
-    
+
       def for_heroku(yaml_file, scope=nil)
         vars = YAML.load(File.open(yaml_file))
-      
+
         heroku_string = convert_vars_to_string(vars)
         heroku_string += convert_vars_to_string(vars.fetch(scope)) if scope.present?
         heroku_string
       end
-    
+
       private
-    
+
       def convert_to_env_vars(vars)
         return unless vars.present?
-        
+
         vars.each do |key, value|
           ENV[key] = value unless value.respond_to?(:each) || value.nil?
         end
       end
-    
+
       def convert_vars_to_string(vars)
         return unless vars.present?
-        
+
         string = ""
         vars.each do |key, value|
           string.concat("#{key}=#{value} ") unless value.respond_to?(:each) || value.nil?
         end
         string
-      end 
+      end
     end
   end
 end
--- spec/loader_spec.rb
+++ spec/loader_spec.rb
@@ -2,66 +2,71 @@ require_relative '../lib/secrets/loader'
 
 describe Secrets::Loader do
   subject { Secrets::Loader }
-  
+
   before do
     @test_secrets = File.expand_path("../fixtures/test_secrets.yml", __FILE__)
     @empty_secrets = File.expand_path("../fixtures/empty_secrets.yml", __FILE__)
+    @nonexistant_secrets = File.expand_path("../fixtures/nonexistant_secrets.yml", __FILE__)
   end
-  
+
   describe "environmentalizing" do
     it "environmentalizes all top-level variables" do
       subject.environmentalize!(@test_secrets)
-      
+
       ENV["FAVORITE_COLOR"].should == "Rainbow"
       ENV["MOST_AWESOME_NINJA_TURTLE"].should == "Donatello"
     end
-    
+
+    it "doesn't barf when the secrets file is missing" do
+      expect { subject.environmentalize!(@nonexistant_secrets) }.not_to raise_error
+    end
+
     it "doesn't barf when given an empty variable" do
       expect { subject.environmentalize!(@test_secrets) }.not_to raise_error
     end
-    
+
     it "doesn't barf when given an empty file" do
       expect { subject.environmentalize!(@empty_secrets) }.not_to raise_error
     end
-    
+
     context "when given a scope" do
       it "environmentalizes variables in the scope" do
         subject.environmentalize!(@test_secrets, "fools")
-        
+
         ENV["FIRST_FOOL"].should == "Feste"
         ENV["SECOND_FOOL"].should == "Patchface"
       end
-      
+
       it "doesn't barf when given a scope filled with empty variables" do
         expect { subject.environmentalize!(@test_secrets, "filled_with_empty") }.not_to raise_error
       end
-      
+
       it "doesn't barf when given an empty scope" do
         expect { subject.environmentalize!(@test_secrets, "empty_scope") }.not_to raise_error
       end
-      
+
       it "still environmentalizes top-level variables" do
         ENV["FAVORITE_COLOR"] = nil
-        
+
         subject.environmentalize!(@test_secrets, "fools")
         ENV["FAVORITE_COLOR"].should be_present
       end
-      
+
       it "does not environmentalize other scopes" do
         ENV["FIRST_FOOL"] = nil
-        
+
         subject.environmentalize!(@test_secrets, "heroes")
         ENV["FIRST_FOOL"].should_not be_present
       end
     end
   end
-  
+
   describe "Heroku string:" do
     it "can return a string suitable for adding vars to Heroku" do
       heroku_string = subject.for_heroku(@test_secrets, "fools")
       heroku_string.should == "FAVORITE_COLOR=Rainbow MOST_AWESOME_NINJA_TURTLE=Donatello FIRST_FOOL=Feste SECOND_FOOL=Patchface "
     end
-    
+
     it "does not include empty scopes in the string" do
       heroku_string = subject.for_heroku(@test_secrets, "fools")
       heroku_string.should_not include "empty_scope"
